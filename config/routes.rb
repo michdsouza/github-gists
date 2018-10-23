d873472c2d9b2b13067b621d6ace112cb15c7721
michdsouza <michd2005@gmail.com>
2018-10-23 19:47:14 -0300

    Set up github omniauth

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get "/auth/:provider/callback", to: "sessions#create"
+  get 'auth/failure', to: redirect('/')
+  delete 'signout', to: 'sessions#destroy', as: 'signout'
+
+  root to: 'sessions#new'
+end
+
+
+
+
+

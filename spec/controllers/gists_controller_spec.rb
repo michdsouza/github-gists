65016307511cd3ff6eb564826c64f5bce0c6454c
michdsouza <michd2005@gmail.com>
2018-10-24 13:33:18 -0300

    Added rspec and first controller test

---
+++ spec/controllers/gists_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe GistsController do
+  let(:user) { FactoryGirl.build(:user) }
+
+  describe "GET index" do
+    before do
+      allow(controller).to receive_messages(current_user: user)
+    end
+
+    it 'response is a success' do
+      get :index
+      expect(response).to be_success
+    end
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :user do
+    email "abc@def.ghi"
+  end
+end
